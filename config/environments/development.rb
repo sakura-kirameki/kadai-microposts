21d6189eb128a5661cfc8f541cabc785ceec7605
sakura.kirameki <sakura.kirameki.x@gmail.com>
2022-09-25 10:50:59 +0000

    clear hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,9 +70,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-end
-
-Rails.application.configure do
-  (省略)
   config.hosts.clear
 end
+
